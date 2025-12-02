8a4265737fa426445828183ba79598e108555a90
MRamirez202210582 <U202210582@upc.edu.pe>
2025-12-02 13:23:20 -0500

    Feat(main):Addm update and delete test

--- ClienteTest/UnitTest1.cs
+++ ClienteTest/UnitTest1.cs
@@ -25,10 +25,16 @@ namespace ClienteTest
             queryServices =new ClienteQueryServices(dbContext);
         }
 
+        [TearDown]
+        public void TearDown()
+        {
+            dbContext.Dispose();
+        }
+
         [Test]
         public void InsertCliente()
         {
-            commandServices.InsertCliente("Rick","Sanchez",30,"Alburquerque","EEUU","California",963852741,"rsanchez@gmail.com",41847596);
+            commandServices.InsertCliente("Fabricio", "Espinoza", 18, "AV.SON BORJA", "Chorrillos", "Lima", 963852741, "fespinozagmail.com", 74859612);
             var cliente=queryServices.GetCliente();
             Assert.AreEqual("Rick", cliente[0].nombreCliente);
         }
@@ -40,18 +46,36 @@ namespace ClienteTest
             dbContext.Cliente.Add(cliente);
             dbContext.SaveChanges();
 
+            var resultado=queryServices.GetCliente().ToList();
+
+            Assert.AreEqual("Fabricio", resultado[0].nombreCliente);
         }
 
         [Test]
         public void UpdateCliente()
         {
+            var cliente = new Cliente { nombreCliente = "Fabricio", apellidoCliente = "Espinoza", edad = 18, direccion = "AV.SON BORJA", pais = "Chorrillos", ciudad = "Lima", telefono = 963852741, correo = "fespinozagmail.com", DNI = 74859612 };
+            dbContext.Cliente.Add(cliente);
+            dbContext.SaveChanges();
 
+            commandServices.EditCliente(cliente.clienteId,"Omar","Zavaleta",20,"av.san isidro","peru","lima",963741852,"ozavaleta@gmail.com",74859632);
+
+            var actulizado = queryServices.GetCliente().ToList();
+
+            Assert.AreEqual("Omar", actulizado[0].nombreCliente);
         }
 
         [Test]
         public void DeleteCliente()
         {
+            var cliente = new Cliente { nombreCliente = "Fabricio", apellidoCliente = "Espinoza", edad = 18, direccion = "AV.SON BORJA", pais = "Chorrillos", ciudad = "Lima", telefono = 963852741, correo = "fespinozagmail.com", DNI = 74859612 };
+            dbContext.Cliente.Add(cliente);
+            dbContext.SaveChanges();
 
+            commandServices.DeleteCliente(cliente.clienteId);
+
+            var resultado = queryServices.GetCliente();
+            Assert.AreEqual(0, resultado.Count);    
         }
 
     }
